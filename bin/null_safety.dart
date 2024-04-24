0cf1235ec2ead620be132cf3e508c56580cb6eda
EevnxX <miftamartin1@gmail.com>
2024-04-24 09:34:36 -0700

    Belajar for dan null safety

---
+++ bin/null_safety.dart
@@ -0,0 +1,36 @@
+void main() {
+  late int? age = null;
+  age = 1;
+
+  if (age != null) {
+    print(age.toDouble());
+  }
+
+  // Konversi Non Nullable ke Nullable
+  String name = 'Mifta';
+  String? nullableName = name;
+  print(nullableName);
+
+  // Konversi Nullable ke Non Nullable
+  int? nullableNumber;
+  if (nullableNumber != null) {
+    int number = nullableNumber;
+    print(number);
+  }
+
+  String? guest = 'Miftahul Huda';
+  var guestName = guest ?? 'Guest';
+  print(guestName);
+
+  int? nullableNumber1 = 1;
+  var number = nullableNumber1!;
+  print(number);
+
+  int? intNumber;
+  double? doubleNumber = intNumber?.toDouble();
+  print(doubleNumber);
+
+  for (var counter = 1; counter <= 10; counter++) {
+    print('Perulangan Ke-$counter');
+  }
+}
