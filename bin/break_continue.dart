fc55dd6a6d363b82a41b063b10e00f79d8c21196
EevnxX <miftamartin1@gmail.com>
2024-04-26 07:47:36 -0700

    ubah break_continue

--- bin/break_continue.dart
+++ bin/break_continue.dart
@@ -1,34 +1,21 @@
 void main() {
   var counter = 1;
-<<<<<<< HEAD
+
   while (true) {
-    print('Urutan Ke-$counter');
-    counter++;
-
-    if (counter >= 10) {
-      break;
-    }
-  }
-}
-=======
-
-  while(true) {
     print('Perulangan Ke-$counter');
     counter++;
 
-    if(counter > 10) {
+    if (counter > 10) {
       break;
     }
   }
 
   var counter2 = 1;
-  for(counter2; counter2 <= 10; counter2++) {
-    if(counter2 % 2 != 1) {
+  for (counter2; counter2 <= 10; counter2++) {
+    if (counter2 % 2 != 1) {
       continue;
     } else {
       print('Perulangan ganjil Ke-$counter2');
     }
-
   }
 }
->>>>>>> f01ead0
